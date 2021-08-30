ddb2ff4de05cd07472b4295d2c004907d1004321
Lucas Oliveira <0liv3ir4.luc4s@gmail.com>
2021-08-30 16:22:54 -0300

    feat: Add table telefone

--- db/food-e.sql
+++ db/food-e.sql
@@ -1,1 +1,9 @@
 CREATE SCHEMA IF NOT EXISTS food_e;
+
+CREATE TABLE IF NOT EXISTS food_e.telefone(
+    id int,
+    ddi INT(3) NOT NULL,
+    ddd INT(3) NOT NULL,
+    numero INT(9) NOT NULL,
+    PRIMARY KEY(id) 
+);
